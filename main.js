46821ea0dc8548a803593784f4c7ca5874b0565f
Kevin Marquez <kmarrufo62@gmail.com>
2025-09-14 17:52:06 -0500

    The restructuring of the HTML and CSS styles has been completed, leaving everything ready for the merge.

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-const emailIcon = document.querySelector('#email');
-const emailIcon2 = document.querySelector('#email2');
-const btnYT = document.querySelectorAll('.yt');
+const emailIcon = document.querySelector("#email");
+const emailIcon2 = document.querySelector("#email2");
+const btnYT = document.querySelectorAll(".youtube");
 
-emailIcon.addEventListener('click', () => {
-    const email_address = 'kmarrufo62@gmail.com';
+emailIcon.addEventListener("click", () => {
+  const email_address = "kmarrufo62@gmail.com";
 
-    navigator.clipboard.writeText(email_address)
-        .then(() => {
-            alert(`Has copiado mi correo electrónico ( ${email_address} ) en el portapapeles.`);
-        })
-        .catch(error => {
-            console.error('Error al copiar: ', error);
-            alert(`No se pudo copiar el email. Puedes copiarlo manualmente: ${email_address}`);
-        });
-})
-
-emailIcon2.addEventListener('click', () => {
-    const email_address = 'kmarrufo62@gmail.com';
-
-    navigator.clipboard.writeText(email_address)
-        .then(() => {
-            alert(`Has copiado mi correo electrónico ( ${email_address} ) en el portapapeles.`);
-        })
-        .catch(error => {
-            console.error('Error al copiar: ', error);
-            alert(`No se pudo copiar el email. Puedes copiarlo manualmente: ${email_address}`);
-        });
-})
-
-btnYT.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        alert("Por el momento, el video para explicar el proyecto se encuentra en grabación/edición.");
+  navigator.clipboard
+    .writeText(email_address)
+    .then(() => {
+      alert(
+        `Has copiado mi correo electrónico ( ${email_address} ) en el portapapeles.`
+      );
     })
-})
+    .catch((error) => {
+      console.error("Error al copiar: ", error);
+      alert(
+        `No se pudo copiar el email. Puedes copiarlo manualmente: ${email_address}`
+      );
+    });
+});
+
+emailIcon2.addEventListener("click", () => {
+  const email_address = "kmarrufo62@gmail.com";
+
+  navigator.clipboard
+    .writeText(email_address)
+    .then(() => {
+      alert(
+        `Has copiado mi correo electrónico ( ${email_address} ) en el portapapeles.`
+      );
+    })
+    .catch((error) => {
+      console.error("Error al copiar: ", error);
+      alert(
+        `No se pudo copiar el email. Puedes copiarlo manualmente: ${email_address}`
+      );
+    });
+});
+
+btnYT.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    alert(
+      "Por el momento, el video para explicar el proyecto se encuentra en grabación/edición."
+    );
+  });
+});
